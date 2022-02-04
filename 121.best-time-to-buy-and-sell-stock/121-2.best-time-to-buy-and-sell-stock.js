886c141ecef176a551229a54dd903041521f6990
Rowen <migodins322@gmail.com>
2022-02-04 21:46:07 +0800

    add 121-2.best-time-to-buy-and-sell-stock

---
+++ 121.best-time-to-buy-and-sell-stock/121-2.best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=121 lang=javascript
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+	let profit = 0;
+	let buy = Number.MAX_SAFE_INTEGER;
+
+	for (let i = 0; i <= prices.length; i++) {
+		if (prices[i] < buy) {
+			buy = prices[i];
+		} else if (prices[i] - buy > profit) {
+			profit = prices[i] - buy;
+		}
+	}
+
+	return profit;
+};
+// @lc code=end
