f85debeae30f9e49c38ddee753bb6dd65ebad92c
Rowen <migodins322@gmail.com>
2022-02-08 13:30:33 +0800

    add 144-2.binary-tree-preorder-traversal

---
+++ 144.binary-tree-preorder-traversal/144-2.binary-tree-preorder-traversal.js
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=144 lang=javascript
+ *
+ * [144] Binary Tree Preorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+	// let stack = [];
+	// let res = [];
+
+	// while (root || stack.length) {
+	// 	if (root) {
+	// 		res.push(root.val);
+	// 		stack.push(root);
+	// 		root = root.left;
+	// 	} else {
+	// 		root = stack.pop();
+	// 		root = root.right;
+	// 	}
+	// }
+
+	// return res;
+	let res = [];
+	traversal(root, res);
+	return res;
+};
+
+const traversal = (root, res) => {
+	if (!root) return res;
+	res.push(root.val);
+	traversal(root.left, res);
+	traversal(root.right, res);
+};
+// @lc code=end
